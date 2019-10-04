ab7f8e5c6996c7783988f20f623fa0f0396cdaed
Waldes <vovan.sushka@gmail.com>
2019-10-04 16:55:37 +0200

    Firs commit

---
+++ New.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// let users = ["anton", "ivan", "petro"];
+// console.log(users[0]);
+//
+// let answer = confirm("Are you ok?");
+// if(answer){
+//     alert("Have a good day!");
+// }else{
+//     let answer2 = prompt("Do you have 18?", "Yes");
+//     if (answer2 === "Yes"){
+//         alert("Have a good day!");
+//     }else{
+//         alert("Go to hell, bitch!!!")
+//     }
+// }
+
